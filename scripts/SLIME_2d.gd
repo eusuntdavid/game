836772a00b6df8e81c7461c1dbb1c0872242900f
David <martondavid383@gmail.com>
2025-12-03 22:41:47 +0200

    full enemy move and collision

--- scripts/SLIME_2d.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	position.x += 1
--- slime.gd
+++ slime.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
+
+var direction = 1
+@onready var ray_cast_2d_right: RayCast2D = $RayCast2DRight
+@onready var ray_cast_2d_left: RayCast2D = $RayCast2DLeft
+
+
 const SPEED = 60
-
-var direction = -1
-
 # Called every fram. "delta"  is the elapsed time since the previous frame
 func _process(delta):
+	
+	if  ray_cast_2d_right.is_colliding():
+		direction = 1
+		print("direction changed right")		
+	if ray_cast_2d_left.is_colliding():
+		direction = -1
+		print("direction changed ;eft")	
 		
-	position.x += direction + SPEED + delta
+		
+	position.x += direction * delta *  SPEED
